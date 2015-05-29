6e6988d0e0e5d5bdd7485d4c081993756a7e00df
Shengyou Fan <shengyoufan@gmail.com>
2015-05-29 18:08:06 +0800

    更新：設定應用園式 Route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,6 +2,15 @@
 
 /*
 |--------------------------------------------------------------------------
+| Route Patterns
+|--------------------------------------------------------------------------
+| 
+*/
+
+Route::pattern('id', '[0-9]+');
+
+/*
+|--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
@@ -11,11 +20,76 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', ['as' => 'home.index', function()
+{
+    return 'home.index';
+}]);
 
-Route::get('home', 'HomeController@index');
+Route::get('about', ['as' => 'about.index', function()
+{
+    return 'about.index';
+}]);
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+Route::get('posts', ['as' => 'posts.index', function()
+{
+    return 'posts.index';
+}]);
+
+Route::get('random', ['as' => 'posts.random', function()
+{
+    return 'posts.random';
+}]);
+
+Route::get('posts/{id}', ['as' => 'posts.show', function($id)
+{
+    return 'posts.show: '.$id;
+}]);
+
+Route::get('posts/create', ['as' => 'posts.create', function()
+{
+    return 'posts.create';
+}]);
+
+Route::post('posts', ['as' => 'posts.store', function()
+{
+    return 'posts.store';
+}]);
+
+Route::get('posts/{id}/edit', ['as' => 'posts.edit', function($id)
+{
+    return 'posts.edit: '.$id;
+}]);
+
+Route::patch('posts/{id}', ['as' => 'posts.update', function($id)
+{
+    return 'posts.update: '.$id;
+}]);
+
+Route::delete('posts/{id}', ['as' => 'posts.destroy', function($id)
+{
+    return 'posts.destroy: '.$id;
+}]);
+
+Route::post('posts/{id}/comment', ['as' => 'posts.comment', function($id)
+{
+    return 'posts.comment: '.$id;
+}]);
+
+Route::get('contact', ['as' => 'contacts.create', function()
+{
+	return 'contact.create';
+}]);
+
+Route::post('contact', ['as' => 'contacts.store', function()
+{
+	return 'contact.store';
+}]);
+
+// Route::get('/', 'WelcomeController@index');
+
+// Route::get('home', 'HomeController@index');
+
+// Route::controllers([
+	// 'auth' => 'Auth\AuthController',
+	// 'password' => 'Auth\PasswordController',
+// ]);
